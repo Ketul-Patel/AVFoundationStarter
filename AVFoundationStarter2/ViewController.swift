eb7371ce7ddcbd7ba65374e77f34df1aa210c28a
Ketul Patel <kjpatel2010@gmail.com>
2015-11-18 15:14:39 -0800

    Initial Commit

---
+++ AVFoundationStarter2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AVFoundationStarter2
+//
+//  Created by Ketul Patel on 11/18/15.
+//  Copyright © 2015 Ketul Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ AVFoundationStarter2Tests/AVFoundationStarter2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  AVFoundationStarter2Tests.swift
+//  AVFoundationStarter2Tests
+//
+//  Created by Ketul Patel on 11/18/15.
+//  Copyright © 2015 Ketul Patel. All rights reserved.
+//
+
+import XCTest
+@testable import AVFoundationStarter2
+
+class AVFoundationStarter2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
